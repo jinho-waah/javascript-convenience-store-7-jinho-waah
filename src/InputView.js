e047edde48d89ad342bbffff1d606ca16db899f6
jinho-waah <andrewjo950515@gmail.com>
2024-11-11 23:21:18 +0900

    fix: bug fix

--- src/InputView.js
+++ src/InputView.js
@@ -9,14 +9,20 @@ export const InputView = {
         );
 
         const list = input.split(",");
-        // 형식이 잘못되었을 때 throw로 예외 발생
         if (list.length === 1 && (input.match(/\[/g) || []).length > 1) {
           throw new Error(
             "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."
           );
         }
 
-        const items = input.match(/\[(.*?)\]/g).map((item) => {
+        const matches = input.match(/\[(.*?)\]/g);
+        if (!matches) {
+          throw new Error(
+            "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."
+          );
+        }
+
+        const items = matches.map((item) => {
           const [name, quantity] = item.replace(/[\[\]]/g, "").split("-");
           if (!name || isNaN(parseInt(quantity))) {
             throw new Error(
