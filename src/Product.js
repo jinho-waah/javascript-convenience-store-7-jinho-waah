92483b9019466a04c8a82b8e4685369b7cdf344d
jinho-waah <andrewjo950515@gmail.com>
2024-11-11 20:37:51 +0900

    refactor: change error message

--- src/Product.js
+++ src/Product.js
@@ -7,7 +7,10 @@ class Product {
   }
 
   reduceQuantity(count) {
-    if (this.quantity < count) throw new Error("재고가 부족합니다.");
+    if (this.quantity < count)
+      throw new Error(
+        "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."
+      );
     this.quantity -= count;
   }
 
