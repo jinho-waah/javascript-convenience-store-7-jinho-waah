751eac7747ff1e1a00aac7e3a0a3459041c06dc1
jinho-waah <andrewjo950515@gmail.com>
2024-11-11 19:20:20 +0900

    fix: minor bug fix

--- src/InputView.js
+++ src/InputView.js
@@ -10,13 +10,17 @@ export const InputView = {
         const items = input.match(/\[(.*?)\]/g).map((item) => {
           const [name, quantity] = item.replace(/[\[\]]/g, "").split("-");
           if (!name || isNaN(parseInt(quantity))) {
-            throw new Error("[ERROR] 잘못된 형식입니다. 다시 입력해 주세요.");
+            throw new Error(
+              "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."
+            );
           }
           return { name, quantity: parseInt(quantity) };
         });
         return items;
       } catch (error) {
-        throw new Error("[ERROR] 잘못된 형식입니다. 다시 입력해 주세요.");
+        throw new Error(
+          "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."
+        );
       }
     }
   },
